a8af9fb8257dbed16b7db6c53880bf9c777942e8
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2022-01-07 15:51:54 +0530

    Initial Commit

---
+++ MapAppSwiftFull/MapAppSwiftFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftFullApp.swift
+//  MapAppSwiftFull
+//
+//  Created by KANISHK VIJAYWARGIYA on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
